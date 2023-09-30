8b7a55a0dae85799c3bd9a81d7433798c6a5efad
0sei531 <oseikofiantwi531@gmail.com>
2023-09-29 18:53:50 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - output each program argument on a separate line..
+ * @argc: length or number of arguments
+ * @argv: argument array
+ *
+ * Return: always 0.
+ */
+int main(int argc, char **argv)
+{
+	int number = 0;
+
+	while (number < argc)
+	{
+		printf("%s\n", *(argv + number));
+		number++;
+		(*argv)++;
+	}
+	return (0);
