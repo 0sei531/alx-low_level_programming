a7d8161fd381159a13ec8ec7265610369dcd9fb3
0sei531 <oseikofiantwi531@gmail.com>
2023-10-05 15:25:46 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
-/**malloc_checked - uses malloc to allocate memory 
+/**
+  * malloc_checked - uses malloc to allocate memory.
   * @b: size of memory to be allocated.
+  *
   * Return: pointer to allocated memory.
   */
 void *malloc_checked(unsigned int b)
