57b05afa6577315e677af6c7fbdf9bdfc0498490
0sei531 <oseikofiantwi531@gmail.com>
2023-10-09 15:28:00 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+/**
+ * Description:function-like macro SUM(x, y)
+ * that computes the sum of the numbers
+ */
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
