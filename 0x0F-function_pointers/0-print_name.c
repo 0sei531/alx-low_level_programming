4d8576c83b874020e4adadf8d5979e844cd3f97c
0sei531 <oseikofiantwi531@gmail.com>
2023-10-11 18:15:48 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
+ * print_name - name printer.
  * @name: input name.
  * @f: function pointer.
  *
@@ -9,6 +9,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
