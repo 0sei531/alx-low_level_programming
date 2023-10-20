c53b831edd007745c5438d53cfd45c07e056bc31
0sei531 <oseikofiantwi531@gmail.com>
2023-10-19 18:43:32 -0700

    b

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,6 @@
-#include "lists.h"
-#include <string.h>
-#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
 /**
  * add_node - creates a new node at the start of a linked list.
@@ -14,7 +13,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *new;
+	list_t *new;
 	signed int len = 0;
 
 	while (str[len])
