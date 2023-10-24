56e80ea1aa31eee03c1e7145d41a48f9f49cc3ec
0sei531 <oseikofiantwi531@gmail.com>
2023-10-24 07:00:47 -0700

    add

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,18 +4,18 @@
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: A pointer to the list_t list's head.
- *
+ * 
  * Return:  numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
 	}
 	return (nodes);
 }
