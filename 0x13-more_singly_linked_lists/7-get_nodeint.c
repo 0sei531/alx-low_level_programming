2a0eb25cea00eb7eb81cfef002f1773f82b86374
0sei531 <oseikofiantwi531@gmail.com>
2023-10-23 20:02:11 -0700

    lyn

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,23 +4,20 @@
  * get_nodeint_at_index - finds a specified node of
  * a listint_t linked list.
  * @head: A pointer to the head of the listint_t list.
- * @index: The index of the node to find 
+ * @index: The index of the node to find
  *
  * Return: the nth node of a listint_t linked list.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node;
-	listint_t *brf;
 
-	brf = head;
-	while (node != index && brf != NULL)
+	for (node = 0; node < index; node++)
 	{
-		brf = brf->next;
-		node++;
+		if (!head)
+			return (NULL);
+
+		head = head->next;
 	}
 
-	if (node != index)
-		return (NULL);
-	return (brf);
 }
