a77767fcc1c5cd9e827b3738e28e071f56e2e08d
0sei531 <oseikofiantwi531@gmail.com>
2023-10-23 22:17:30 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
  * print_listint - Prints all the elements of a listint_t list.
  * @h: A pointer to the list_t list's head.
  *
- * Return: The nodes value in the list_t list.
+ * Return:  numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
