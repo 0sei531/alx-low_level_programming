dc26c100e08df17a35cb948976f76eaf7044ff32
0sei531 <oseikofiantwi531@gmail.com>
2023-10-26 17:24:51 -0700

    lyn

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int ama;
-	unsigned long int ponkoh;
+	unsigned long int ponkoh = 0;
 	ama = n ^ m;
 
 	while (ama > 0)
