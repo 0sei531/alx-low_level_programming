cbaf7c5a45f7df254dd1118491a12e3befc929dc
0sei531 <oseikofiantwi531@gmail.com>
2023-10-26 17:35:22 -0700

    lyn

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
+
 /**
  * flip_bits - number of bits you would need to flip
  * to get from one number to another
  * @n: parameter
- * @m: second value 
+ * @m: second value
  * Return: ponkoh
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int ama;
 	unsigned long int ponkoh = 0;
+
 	ama = n ^ m;
 
 	while (ama > 0)
