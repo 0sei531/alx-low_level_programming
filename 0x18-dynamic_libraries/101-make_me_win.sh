125b108ae2973c517ce54713f80242b41df1aade
0sei531 <oseikofiantwi531@gmail.com>
2023-12-11 16:11:40 -0800

    f

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/0sei531/alx-low_level_programming/master/0x18-dynamic_libraries/libtry.so
-export LD_PRELOAD="$PWD/../libtry.so"
+wget -P /tmp/ https://raw.github.com/0sei531/alx-low_level_programming/master/0x18-dynamic_libraries/libtry.so
+export LD_PRELOAD =/tmp/libtry.so
